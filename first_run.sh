f95d461ae7309849e01e045d33bd5aca67039b53
Martim Lobao <martim@elios.technology>
2019-02-10 14:30:18 -0500

    install black on first run

--- first_run.sh
+++ first_run.sh
@@ -105,6 +105,7 @@ brew cask install qlstephen
 
 brew install python
 pip install virtualenv
+pip install black
 # brew install --ignore-dependencies https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
 # ^^ install python 3.6.5_1
 brew cask install xquartz
