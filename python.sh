df81cdbfb533fddbe14540f0bad52bd5e46d7de3
Martim Lobao <martim@elios.technology>
2019-03-12 02:21:12 -0400

    [cleanup] minor updates to formatting in exports, inputrc, path, and python

--- python.sh
+++ python.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+
 ###############################################################################
 # INSTALL PYTHON, PYENV, CONDA, AND JUPYTER
 ###############################################################################
+
 # Make sure homebrew is installed and the following command has been run:
 # sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 brew install python
