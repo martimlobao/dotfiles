c9a0b8834246d6484a6770c0d369b131c2146d9a
Martim Lobao <martim@elios.technology>
2019-03-12 02:24:51 -0400

    [brew] remove unnecessary formulas and add useful ones

--- brew.sh
+++ brew.sh
@@ -1,97 +1,54 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+###############################################################################
+# INSTALL COMMAND-LINE TOOLS USING HOMEBREW
+###############################################################################
 
-# Make sure we’re using the latest Homebrew.
+# Update Homebrew and installed formulas.
 brew update
-
-# Upgrade any already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Switch to using brew-installed bash as default shell.
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+    chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
+# Install git utilities.
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+
+# Install other languages.
+brew install node
+
+# Install download utilities.
+brew install httpie
+brew install wget
+brew install youtube-dl
+
+# Install fancy shell stuff.
+brew install autojump
+brew install mas
+brew install nnn
+
+# Install very important stuff.
+brew install cowsay
+brew install googler
+brew install lolcat
+brew install neofetch
+brew install thefuck
+
+# Install other useful binaries.
+brew install exiftool
+brew install imagemagick
+brew install pandoc
 
 # Remove outdated versions from the cellar.
 brew cleanup
