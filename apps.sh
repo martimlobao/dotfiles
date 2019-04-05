3cfe60f3a44d01843d10b727e44dccd6db8169e8
Martim Lobao <m@rtimlobao.com>
2019-04-05 19:58:52 -0400

    reduce verbosity and remove insomniax

--- apps.sh
+++ apps.sh
@@ -21,105 +21,54 @@ brew cask install xquartz
 
 # APPLE
 if [[ $WORKPC != true ]]; then
-	echo "Installing Numbers..."
-	mas install 409203825
-	echo "Installing GarageBand..."
-	mas install 682658836
-	echo "Installing iMovie..."
-	mas install 408981434
-	echo "Installing Pages..."
-	mas install 409201541
-	echo "Installing Keynote..."
-	mas install 409183694
+	mas install 409203825 # Numbers
+	mas install 408981434 # iMovie
+	mas install 409201541 # Pages
+	mas install 409183694 # Keynote
 fi
 
 # DESIGN
-echo "Installing LICEcap..."
 brew cask install licecap
-echo "Installing GIF Brewery 3..."
-mas install 1081413713
-# Adobe Acrobat DC
-# Adobe Illustrator CC 2018
-# Adobe InDesign CC 2018
-# Adobe Lightroom Classic CC
-# Adobe Photoshop CC 2018
-# Vector Magic.app
+mas install 1081413713 # GIF Brewery 3
 
 # DEVELOPER
-echo "Installing Atom..."
 brew cask install atom
-echo "Installing Jupyter Notebook Viewer..."
 brew cask install jupyter-notebook-viewer
-echo "Installing MongoDB Compass..."
 brew cask install mongodb-compass
-echo "Installing MySQLWorkbench..."
 brew cask install mysqlworkbench
-echo "Installing Sublime Text..."
 brew cask install sublime-text
-echo "Installing Tableau..."
 brew cask install tableau-public
-# Mathematica.app
-# WolframScript.app
 
 # LEISURE
-echo "Installing Spotify..."
 brew cask install spotify
-echo "Installing Sonos..."
 brew cask install homebrew/cask-drivers/sonos
 if [[ $WORKPC != true ]]; then
-	echo "Installing Minecraft..."
 	brew cask install minecraft
 fi
 
-# OFFICE
-# Microsoft Excel.app
-# Microsoft OneNote.app
-# Microsoft PowerPoint.app
-# Microsoft Word.app
-
 # PRODUCTIVITY
-echo "Installing Dropbox..."
 brew cask install dropbox
-echo "Installing Slack..."
 brew cask install slack # mas install 803453959
 if [[ $WORKPC != true ]]; then
-	echo "Installing Evernote..."
 	brew cask install evernote # mas install 406056744
-	echo "Installing Wunderlist..."
-	mas install 410628904
+	mas install 410628904 # Wunderlist
 fi
 
 # SOCIAL
-echo "Installing Houseparty..."
 brew cask install houseparty # mas install 1381523962
-echo "Installing Skype..."
 brew cask install skype
 if [[ $WORKPC != true ]]; then
-	echo "Installing Telegram..."
 	brew cask install telegram # mas install 747648890
-	echo "Installing WhatsApp..."
 	brew cask install whatsapp # mas install 1147396723
 fi
 
 # UTILITIES
-echo "Installing Google Chrome..."
 brew cask install google-chrome
-echo "Installing InsomniaX..."
-brew cask install insomniax
-echo "Installing The Unarchiver..."
 brew cask install the-unarchiver # mas install 425424353
-echo "Installing Tunnelblick..."
 brew cask install tunnelblick
 if [[ $WORKPC != true ]]; then
-	echo "Installing iStat Menus..."
 	brew cask install istat-menus
-	echo "Installing Transmission..."
 	brew cask install transmission
-	echo "Installing VLC..."
 	brew cask install vlc
-	echo "Installing Wake Up Time..."
-	mas install 495945638
+	mas install 495945638 # Wake Up Time
 fi
-# echo "Installing CleanMyMac X..."
-# brew cask install cleanmymac
-# Parallels Desktop
