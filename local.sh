e0744d733cd5039c18af2a4e059fbafae82cb69c
Martim Lobao <m@rtimlobao.com>
2024-04-16 13:27:15 +0100

    cleanup

--- local.sh
+++ local.sh
@@ -45,14 +45,21 @@ sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.serve
 # op item get --fields="license key" "iStat Menus 6"
 # op read op://private/istat\ menus\ 6/license\ key
 
-# iStat Menus
 # copy all files from manual/ to ~/
 cp -r manual/ ~/
-defaults write com.bjango.istatmenus _modelid -string $(sysctl hw.model | sed 's/hw.model: //')
-defaults write com.bjango.istatmenus installDateV6 -int $(date -v +14d +%s)
-ISTAT_EMAIL=$(op item get --fields="registered email" "iStat Menus 6")
-ISTAT_KEY=$(op item get --fields="license key" "iStat Menus 6")
-/usr/libexec/PlistBuddy -c "Delete :license6" ~/Library/Preferences/com.bjango.istatmenus.plist
-/usr/libexec/PlistBuddy -c "Add :license6 dict" ~/Library/Preferences/com.bjango.istatmenus.plist
-/usr/libexec/PlistBuddy -c "Add :license6:email string $ISTAT_EMAIL" ~/Library/Preferences/com.bjango.istatmenus.plist
-/usr/libexec/PlistBuddy -c "Add :license6:serial string $ISTAT_KEY" ~/Library/Preferences/com.bjango.istatmenus.plist
+
+# iStat Menus
+if ! defaults read com.bjango.istatmenus license6 &> /dev/null; then
+	echo -e "⬇️  \033[1;34mRegistering iStat Menus...\033[0m"
+	defaults write com.bjango.istatmenus _modelid -string $(sysctl hw.model | sed 's/hw.model: //')
+	defaults write com.bjango.istatmenus installDateV6 -int $(date -v +14d +%s)
+
+	ISTAT_EMAIL=$(op item get --fields="registered email" "iStat Menus 6")
+	ISTAT_KEY=$(op item get --fields="license key" "iStat Menus 6")
+	/usr/libexec/PlistBuddy -c "Add :license6 dict" ~/Library/Preferences/com.bjango.istatmenus.plist
+	/usr/libexec/PlistBuddy -c "Add :license6:email string $ISTAT_EMAIL" ~/Library/Preferences/com.bjango.istatmenus.plist
+	/usr/libexec/PlistBuddy -c "Add :license6:serial string $ISTAT_KEY" ~/Library/Preferences/com.bjango.istatmenus.plist
+	echo -e "✅  \033[1;32miStat Menus registered successfully.\033[0m"
+else
+	echo -e "✅  \033[1;32miStat Menus is already registered.\033[0m"
+fi
