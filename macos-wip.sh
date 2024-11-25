432a3c0a446b2087d1211fa12708b4126b378837
Martim Lobao <dev@lobao.io>
2024-11-25 20:24:33 +0000

    add more things

--- macos-wip.sh
+++ macos-wip.sh
@@ -11,7 +11,7 @@ defaultbrowser browser
 * show sound in menu bar
 
 * display resolution
-echo "Select screen resolution and close the Terminal app when done"
+echo "Select screen resolution and close System Settings when done"
 open -W x-apple.systempreferences:com.apple.Displays-Settings.extension
 
 * trackpad tracking speed
@@ -30,7 +30,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCorner
 defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
 
-
 * swipe between full-screen apps with 4 fingers
 defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
@@ -45,11 +44,14 @@ defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 2
 
-
 * screen saver activate
 * turn off display
 * require password after screen saver
 * set screen saver
+echo "Select screensaver and close System Settings when done"
+open -W x-apple.systempreferences:com.apple.ScreenSaver-Settings.extension
+# todo: if you want to automate this more, see: https://forum.iscreensaver.com/t/understanding-the-macos-sonoma-screensaver-plist/718/4
+# convert plist from binary to regular: https://osxdaily.com/2016/03/10/convert-plist-file-xml-binary-mac-os-x-plutil/
 * set wallpaper
 * desktop and dock
 
@@ -59,7 +61,6 @@ defaults write com.apple.dock autohide -bool true
 
 # don't show recents
 # defaults write com.apple.dock show-recents -bool false
-
 
 # System Events got an error: Script Editor is not allowed assistive access.
 
@@ -76,11 +77,9 @@ defaults write com.apple.dock mru-spaces -bool false
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-
 ##########
 # FINDER #
 ##########
-* show all filename extensions
 # Show all filename extensions:
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write -g AppleShowAllExtensions -bool true
@@ -92,7 +91,6 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 # Show macOS system ~/Library hidden by default:
 chflags hidden ~/Library
-
 
 # Show hard drives and network drives on the Desktop
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
@@ -115,6 +113,14 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable automatic capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+# Disable automatic period
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+# Disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 # Calculate all sizes
 /usr/libexec/PlistBuddy -c "Set :'FK_StandardViewSettings':'ExtendedListViewSettingsV2':'calculateAllSizes' 'true'" ~/Library/Preferences/com.apple.finder.plist
@@ -146,7 +152,6 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 defaults read com.apple.finder
-
 
 # https://medium.com/@laclementine/dotfile-for-mac-efe082ad0d6a
 # Get current Username and User ID
@@ -189,5 +194,5 @@ sudo -u "$CURRENT_USER" /usr/libexec/PlistBuddy -c "Set :'StandardViewSettings':
 # https://github.com/br3ndonland/dotfiles/blob/main/scripts/macos.sh
 
 tell application "System Settings"
-	set the current pane to pane id "com.apple.Displays-Settings.extension"
+set the current pane to pane id "com.apple.Displays-Settings.extension"
 end tell
