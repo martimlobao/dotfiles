f73dae03e0101fc36aefef6e6cfda5eb09acc960
Martim Lobao <dev@lobao.io>
2025-02-03 00:12:44 +0000

    improve op login, handle tapped apps from brew, reorder core apps

--- local.sh
+++ local.sh
@@ -14,11 +14,11 @@ echo -e "🔑 \033[1;34mSetting local settings and variables...\033[0m"
 
 # ensure signed in to 1Password
 echo -e "🔐 \033[1;35mSigning in to 1Password...\033[0m"
-if [[ -z $(op account get) ]]; then
-	eval $(op account add --address my.1password.com)
-	eval $(op signin --help)
-	echo "Also set up SSH Agent"
+if [[ -z $(op account get 2>/dev/null) ]]; then
+	echo -e "ℹ️  \033[1;33mSigning in to 1Password for the first time, please follow the instructions and enable the SSH Agent in the 1Password app.\033[0m"
+	op signin --help
 	open /Applications/1Password.app
+	eval $(op account add --address my.1password.com)
 fi
 eval $(op signin)
 
