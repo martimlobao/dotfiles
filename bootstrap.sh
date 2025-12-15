a9b78dc5f56d741b9ec4856cb9c85201cf8c5557
Martim Lobao <dev@lobao.io>
2025-12-15 19:59:13 +0000

    dry

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,20 +17,14 @@
 
 	echo -e "🥾 \033[1;34mBootstrapping dotfiles\033[0m"
 
+	if [[ -z ${BRANCH} ]]; then
+		BRANCH="main"
+	fi
 	if [[ ! -d ${DOTPATH} ]]; then
-		if [[ -z ${BRANCH} ]]; then
-			echo -e "📑 \033[1;33mCloning dotfiles...\033[0m"
-			git clone https://github.com/martimlobao/dotfiles.git "${DOTPATH}"
-			echo -e "✅ \033[1;32mCloned Dotfiles to ${DOTPATH}\033[0m"
-		else
-			echo -e "📑 \033[1;33mCloning dotfiles on branch ${BRANCH}...\033[0m"
-			git clone https://github.com/martimlobao/dotfiles.git --branch "${BRANCH}" "${DOTPATH}"
-			echo -e "✅ \033[1;32mCloned Dotfiles to ${DOTPATH} on branch ${BRANCH}\033[0m"
-		fi
+		echo -e "📑 \033[1;33mCloning dotfiles on branch ${BRANCH}...\033[0m"
+		git clone https://github.com/martimlobao/dotfiles.git --branch "${BRANCH}" "${DOTPATH}"
+		echo -e "✅ \033[1;32mCloned Dotfiles to ${DOTPATH} on branch \"${BRANCH}\"\033[0m"
 	else
-		if [[ -z ${BRANCH} ]]; then
-			BRANCH="main"
-		fi
 		echo -e "✅ \033[1;34mDotfiles already downloaded to ${DOTPATH}, checking out branch \"${BRANCH}\"\033[0m"
 		cd "${DOTPATH}"
 		if [[ $(git status -s) ]]; then
@@ -38,7 +32,7 @@
 			git stash save "stash created automatically on $(date) by bootstrap.sh"
 		fi
 		git checkout "${BRANCH}"
-		git pull origin "${BRANCH}"
+		git pull --ff-only origin "${BRANCH}"
 	fi
 
 	cd "${DOTPATH}"
